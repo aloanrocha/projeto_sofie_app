92c9d7ac59061bf7a8074494c331e81514b844ee
Aloan Rocha Santos <aloanrocha@MacBook-Pro-de-Aloan.local>
2020-09-09 21:12:30 -0300

    Adicionando o projeto ao git

---
+++ app/src/main/java/com/portfolio/sofieandroid/ActivitySplashScreen.kt
@@ -0,0 +1,12 @@
+package com.portfolio.sofieandroid
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivitySplashScreen : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_screen_splash)
+    }
+}
