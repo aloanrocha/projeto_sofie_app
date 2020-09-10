56cee941c5509292bd805e0233e608b86c6f9649
Aloan Rocha Santos <aloanrocha@MacBook-Pro-de-Aloan.local>
2020-09-10 09:18:54 -0300

    ajuste no layout

--- app/src/main/java/com/portfolio/sofieandroid/ActivityListTarefas.kt
+++ app/src/main/java/com/portfolio/sofieandroid/ActivityListTarefas.kt
@@ -1,13 +1,12 @@
 package com.portfolio.sofieandroid
 
-import android.content.Intent
-import android.support.v7.app.AppCompatActivity
-import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
 import android.content.DialogInterface
+import android.content.Intent
+import android.os.Bundle
 import android.support.v7.app.AlertDialog
+import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
+import android.widget.TextView
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_tarefas.*
 
